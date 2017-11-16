2abaf97b471301472a5c1ab00625ab2995e1ba63
mastergray <me@mastergray.com>
2017-11-15 22:08:11 -0600

    Update Functional.js

--- Functional.js
+++ Functional.js
@@ -4,46 +4,58 @@
  *
  */
 
-Functional = {
-  
-  // :: Transforms a polyadic function into an unary function:
-  curry : function (fn) {
-    
-    var _arity = fn.length,             // number of arguments to reduce
-        _slice = Array.prototype.slice; // transforms array-like object into an array
-    
-    // Evaluates function once number of arguments is equal or greater to arity:
-    function reduceArity() {
+function () {
 
-      var _args = _slice.call(arguments);
-      
-      //  Evaulate function, otherwise continue to concat new arguments:
-      return _args.length >= _arity
-        ? fn.apply(null, _args)
-        : function () {
-            return reduceArity.apply(null, _args.concat(_slice.call(arguments)));
-        }
-      
-    }
+  // Transforms a polyadic function into an unary function:
+  function curry(fn) {
     
-    return reduceArity;
+      var _arity = fn.length,             // number of arguments to reduce
+          _slice = Array.prototype.slice; // transforms array-like object into an array
+          
+      // Evaluates function once number of arguments is equal or greater to arity:
+      function reduceArity() {
+        
+        var _args = _slice.call(arguments);
+        
+        //  Evaulate function, otherwise continue to concat new arguments:
+        return _args.length >= _arity
+          ? fn.apply(null, _args)
+          : function () {
+              return reduceArity.apply(null, _args.concat(_slice.call(arguments)));
+          };
+        
+      }
     
-  },
+      return reduceArity;
+  }
   
-  // :: Transforms a list of unary functions into a single unary function
-  compose: function () {
-    
-    // Applies the arguments of one function to call for the arguments of another:
-    var _composeThis = function (x,y) {
+  // Transforms a list of unary functions into a single unary function:
+  function compose (array) {
+    return array.reduce(function (x,y) {
+      // Applies the arguments of one function to call for the arguments of another:
       return function () {
         return y.call(this, x.apply(this, arguments));
-      }
-    }
-    
-    // Reduces list of functions:
-    return Array.prototype.slice.call(arguments).reduce(function (result, fn) {
-      return _composeThis(result,fn);
-    });
+      };
+    // Acclumator starts with an identity function:
+    }, function (x) {
+      return x;
+    })
   }
- 
-}
+
+  //  Returns a single array of elements from an array that may contain other arrays:
+  function flatten (array) {
+    return array.reduce(function (a, b){
+      return Array.isArray(b)
+        ? a.concat(flatten(b))
+        : a.concat(b);
+   }, []);
+  }
+  
+  // Returns a namespace containing all functions from above:
+  return {
+    curry:curry,
+    compose:compose,
+    flatten:flatten
+  };
+
+};
